39fb6953c0ab1487671f1a630b1a565030b96184
Ruggero (Руджеро) Ferretti <Ruggero.Ferretti@acronis.com>
2024-07-05 17:37:00 +0200

    Continued

--- internal/engines/esms/if_shot.go
+++ internal/engines/esms/if_shot.go
@@ -5,20 +5,38 @@ import (
 	"github.com/ilmaruk/esms-go/internal/random"
 )
 
-func IfShot(rnd random.Random, attacking *models.Teamsheet) {
+func IfShot(rnd random.Random, attacking, defending *models.Teamsheet) {
 	// Did a scoring chance occur ?
 	if !rnd.RandomP(attacking.ShotProb) {
 		return
 	}
 
-	var assister *models.Player
+	var assister, shooter *models.Player
 	var chanceAssisted = false
 
 	// There's a 0.75 probability that a chance was assisted, and
 	// 0.25 that it's a solo
 	if !rnd.RandomP(7500) {
+		// Assister
 		assister = WhoDidIt(rnd, attacking, ACTION_ASSIST)
 		chanceAssisted = true
+		assister.KeyPasses++
+
+		// Shooter
 		shooter = WhoGotAssist(rnd, attacking, assister)
+	} else {
+		shooter = WhoDidIt(rnd, attacking, ACTION_SHOT)
+	}
+
+	chanceTackled := (int)(4000.0 * ((defending.TeamTackling * 3.0) / (attacking.TeamPassing*2.0 + attacking.TeamShooting)))
+	if rnd.RandomP(chanceTackled) {
+		tackler := WhoDidIt(rnd, defending, ACTION_TACKLE)
+		tackler.Tackles++
+		return
+	}
+
+	if !OnTarget(rnd, attacking, shooter) {
+		shooter.ShotsOff++
+		attacking.FinalShotsOff++
 	}
 }
